344b87dbc5534389fd7390c281822f20421b61a0
Colette J Beckwith <61260684+colettejbeckwith@users.noreply.github.com>
2025-11-24 21:27:39 -0600

    fully working project. Score is working, the game itself is working, and it is demonstrating all the functionality desired.

--- javascript.js
+++ javascript.js
@@ -1,72 +1,153 @@
+const humanScoreDiv = document.querySelector("#human-actual-score-number");
+const computerScoreDiv = document.querySelector("#computer-actual-score-number");
+const humanCardDiv = document.querySelector("#human-card");
+const humanElement = document.getElementById("human-card");
+const computerCardDiv = document.querySelector("#computer-card");
+const computerElement = document.getElementById("computer-card");
+const resultDiv = document.querySelector("#result");
+const resultElement = document.getElementById("result");
+
+let humanScore = 0;
+let computerScore = 0;
+
+computerScoreDiv.textContent = computerScore;
+humanScoreDiv.textContent = humanScore;
+
 playGame();
 
 function getComputerChoice() {
     let compChoice = Math.random();
     if (compChoice <= 0.33) {
+        computerCardDiv.textContent = "ROCK";
+        computerElement.style.backgroundColor = "sandybrown";
         return "ROCK"
     } else if (compChoice <= 0.66) {
+        computerCardDiv.textContent = "PAPER";
+        computerElement.style.backgroundColor = "rgb(242, 220, 192)";
         return "PAPER"
     } else {
+        computerCardDiv.textContent = "SCISSORS";
+        computerElement.style.backgroundColor = "silver";
         return "SCISSORS"
     }
-}
-
-function getHumanChoice() {
-    // return prompt("Please enter rock, paper, or scissors.").toUpperCase();
-
-    
 }
 
 function playRound(computerChoice, humanChoice) {
     if (computerChoice == "ROCK") {
         if (humanChoice == "ROCK") { // TIE
-            console.log("Tie! Try Again!");
-            return "TIE";
+            resultDiv.textContent = "Tie!";
+            resultElement.style.color = black;
+            humanCardDiv.textContent = "ROCK";
+            humanElement.style.backgroundColor = "sandybrown";
         } else if (humanChoice == "PAPER") {
-            console.log("You win! Paper beats rock!");
-            return "HUMAN";
+            humanCardDiv.textContent = "PAPER";
+            resultElement.style.color = "green";
+            humanScore++;
+            humanScoreDiv.textContent = humanScore;
+            humanElement.style.backgroundColor = "rgb(242, 220, 192)";
+            resultDiv.textContent = "Round won! +1 point!";
         } else if (humanChoice == "SCISSORS") {
-            console.log("You lose! Rock beats scissors!");
-            return "COMPUTER";
+            humanCardDiv.textContent = "SCISSORS";
+            resultElement.style.color = "red";
+            computerScore++;
+            computerScoreDiv.textContent = computerScore;
+            humanElement.style.backgroundColor = "silver";
+            resultDiv.textContent = "Round lost! +1 point for computer!";
         }
     } else if (computerChoice == "PAPER") {
         if (humanChoice == "PAPER") { // TIE
-            console.log("Tie! Try Again!");
-            return "TIE";
+            resultDiv.textContent = "Tie!";
+            resultElement.style.color = "black";
+            humanCardDiv.textContent = "PAPER";
+            humanElement.style.backgroundColor = "rgb(242, 220, 192)";
         } else if (humanChoice == "SCISSORS") {
-            console.log("You win! Scissors beats paper!");
-            return "HUMAN";
+            humanCardDiv.textContent = "SCISSORS";
+            resultElement.style.color = "green";
+            humanScore++;
+            humanScoreDiv.textContent = humanScore;
+            humanElement.style.backgroundColor = "silver";
+            resultDiv.textContent = "Round won! +1 point!";
         } else if (humanChoice == "ROCK") {
-            console.log("You lose! Paper beats rock!");
-            return "COMPUTER";
+            humanCardDiv.textContent = "ROCK";
+            resultElement.style.color = "red";
+            computerScore++;
+            computerScoreDiv.textContent = computerScore;
+            humanElement.style.backgroundColor = "sandybrown";
+            resultDiv.textContent = "Round lost! +1 point for computer!";
         }
     } else if (computerChoice == "SCISSORS") {
         if (humanChoice == "SCISSORS") { // TIE
-            console.log("Tie! Try Again!");
-            return "TIE";
+            humanCardDiv.textContent = "SCISSORS";
+            resultElement.style.color = "black";
+            humanElement.style.backgroundColor = "silver";
+            resultDiv.textContent = "Tie!"
         } else if (humanChoice == "ROCK") {
-            console.log ("You win! Rock beats scissors!");
-            return "HUMAN";
+            humanCardDiv.textContent = "ROCK";
+            resultElement.style.color = "green";
+            humanScore++;
+            humanScoreDiv.textContent = humanScore;
+            humanElement.style.backgroundColor = "sandybrown";
+            resultDiv.textContent = "Round won! +1 point!";
         } else if (humanChoice == "PAPER") {
-            console.log("You lose! Scissors beats paper!");
-            return "COMPUTER";
+            humanCardDiv.textContent = "PAPER";
+            resultElement.style.color = "red";
+            computerScore++;
+            computerScoreDiv.textContent = computerScore;
+            humanElement.style.backgroundColor = "rgb(242, 220, 192)";
+            resultDiv.textContent = "Round lost! +1 point for computer!";
         }
+    }
+    checkForVictory();
+}
+
+function handleRockClick(humanChoice) {
+    return playRound(getComputerChoice(), "ROCK");
+}
+
+function handlePaperClick(humanChoice) {
+    return playRound(getComputerChoice(), "PAPER");
+}
+
+function handleScissorsClick(humanChoice) {
+    return playRound(getComputerChoice(), "SCISSORS");
+}
+
+function checkForVictory() {
+
+    if (humanScore >= 3) {
+        resultDiv.textContent = "You win the game! Congratulations!"
+        humanScore = 0;
+        computerScore = 0;
+        humanScoreDiv.textContent = humanScore;
+        computerScoreDiv.textContent = computerScore;
+        humanCardDiv.textContent = "";
+        computerCardDiv.textContent = "";
+        humanElement.style.backgroundColor = "green";
+        computerElement.style.backgroundColor = "red";
+    }
+
+    if (computerScore >= 3) {
+        resultDiv.textContent = "You lose the game! Sorry!"
+        humanScore = 0;
+        computerScore = 0;
+        humanScoreDiv.textContent = humanScore;
+        computerScoreDiv.textContent = computerScore;
+        humanCardDiv.textContent = "";
+        computerCardDiv.textContent = "";
+        humanElement.style.backgroundColor = "red";
+        computerElement.style.backgroundColor = "green";
     }
 }
 
 function playGame() {
 
-    let humanScore = 0;
-    let computerScore = 0;
+    const rockButton = document.getElementById('rock');
+    const paperButton = document.getElementById('paper');
+    const scissorsButton = document.getElementById('scissors');
 
-    // for (let i = 0; i <= 4; i++) {
-        let result = playRound(getComputerChoice(), getHumanChoice());
-        if (result == "HUMAN") {humanScore++;}
-        if (result == "COMPUTER") {computerScore++;}
-        // if (result == "TIE") {i--;}
-    // }
+    rockButton.addEventListener('click', handleRockClick);
+    paperButton.addEventListener('click', handlePaperClick);
+    scissorsButton.addEventListener('click', handleScissorsClick);
 
-    console.log("Game Over!")
-    console.log("Your score: " + humanScore);
-    console.log("Computer score: " + computerScore);
+    
 }
